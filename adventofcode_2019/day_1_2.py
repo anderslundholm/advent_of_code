e6ed8e83601bb4eedd8f54969c0e58bb4514eb6c
Anders Lundholm <ext.anders.lundholm@king.com>
2019-12-02 16:28:53 +0100

    Solved day 1.

--- adventofcode_2019/day_1_2.py
+++ adventofcode_2019/day_1_2.py
@@ -7,10 +7,15 @@ def round_down(n, decimals=0):
 
 
 def calc_fuel_required(mass):
-    return round_down(mass/3)-2
+    total_fuel_req = 0
+    while mass > 0:
+        mass = round_down(mass/3)-2 
+        if mass > 0:
+            total_fuel_req += mass
+    return total_fuel_req
 
 
-def total_fuel_consumption(module_mass_list=[]):
+def total_fuel_consumption(mass_list=[]):
     total = 0
     for mass in module_mass_list:
         total += calc_fuel_required(mass)
@@ -18,7 +23,7 @@ def total_fuel_consumption(module_mass_list=[]):
 
 
 if __name__ == '__main__':
-    mass_list = [
+    module_mass_list = [
         120150, 105328, 70481, 86674, 112434, 94883, 147500, 146007, 103982,
         65758, 132357, 60885, 97516, 96977, 129085, 80330, 124081, 102501,
         102505, 70029, 54155, 69253, 60120, 53192, 89470, 137125, 136296,
@@ -31,4 +36,4 @@ if __name__ == '__main__':
         101115, 61773, 100951, 77189, 119083, 93841, 109090, 83370, 70230,
         144812, 67647, 105358, 135780, 85225, 100697, 100998, 131151, 52826,
         114084]
-    print(total_fuel_consumption(mass_list))
+    print(total_fuel_consumption(module_mass_list))
