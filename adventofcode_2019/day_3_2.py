3f7a3685383423e46ad2775f5d225c1099594071
Anders Lundholm <anders.andlu.se>
2019-12-04 22:25:30 +0100

    Finished day 3.

--- adventofcode_2019/day_3_2.py
+++ adventofcode_2019/day_3_2.py
@@ -18,7 +18,8 @@ def manhattan_distance(crossing_points_set):
 
 def map_wire_path(wire_cordinates):
     steps = set()
-    x, y = 0, 0
+    count_point_dict = {}
+    x, y, counter = 0, 0, 0
     for index, step in enumerate(wire_cordinates):
         for i in range(int(wire_cordinates[index][1:])):
             if step[0] == 'U':
@@ -29,19 +30,25 @@ def map_wire_path(wire_cordinates):
                 y -= 1
             elif step[0] == 'L':
                 x -= 1
+            counter += 1
             steps.add((x, y))
-    return steps
+            count_point_dict[(x, y)] = counter
+    return steps, count_point_dict
 
 
 def compare_wire_paths(input_list):
-    wire_set_1 = map_wire_path(input_list[0])
-    wire_set_2 = map_wire_path(input_list[1])
-    return wire_set_1.intersection(wire_set_2)
+    wire_set_1, count_point_dict_1 = map_wire_path(input_list[0])
+    wire_set_2, count_point_dict_2 = map_wire_path(input_list[1])
+    crossing_points = wire_set_1.intersection(wire_set_2)
+    path_counts = [y + count_point_dict_2[x]
+                   for x, y in count_point_dict_1.items()
+                   if x in crossing_points]
+    return path_counts
 
 
 if __name__ == '__main__':
     input_list = read_input()
-    crossing_points = compare_wire_paths(input_list)
-    distances = manhattan_distance(crossing_points)
-    closest_distance = min(distances)
+    path_counts = compare_wire_paths(input_list)
+    # distances = manhattan_distance(crossing_points)
+    closest_distance = min(path_counts)
     print(closest_distance)
