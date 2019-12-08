d98cabe7c50bd330fd936217b05f69fa67c69c61
Anders Lundholm <anders.andlu.se>
2019-12-08 13:11:04 +0100

    Completed day 4.

--- adventofcode_2019/day_4_2.py
+++ adventofcode_2019/day_4_2.py
@@ -8,30 +8,21 @@ def check_double_digit2(i):
 
 def calc_possible_passwords():
     count = 0
-    adjacent = False
     for number in range(356261, 846303):
         if not check_double_digit(number):
             continue
         number_string = (str(number))
-        for index, digit in enumerate(number_string[:-1]):
-            if int(digit) > int(number_string[index+1]):
-                break
-            if int(digit) == int(number_string[index+1]):
-                if index == 0:
-                    if int(digit) == int(number_string[index+2]):
-                        adjacent = False
-                        break
-                elif (index == len(number_string) - 2 and
-                      int(digit) == int(number_string[index-1])):
-                    adjacent = False
+        adjacent = False
+        for index, digit in enumerate(number_string):
+            if index != 0:
+                if int(digit) < int(number_string[index-1]):
                     break
-                elif int(digit) == int(number_string[index-1]):
-                    adjacent = False
-                    break
-                adjacent = True
-            if adjacent and len(number_string) == index + 2:
-                count += 1
-
+                if (int(digit) == int(number_string[index-1]) and
+                   number_string.count(digit) == 2):
+                    print(number, index, digit)
+                    adjacent = True
+                if adjacent and len(number_string) == index+1:
+                    count += 1
     print(count)
 
 
